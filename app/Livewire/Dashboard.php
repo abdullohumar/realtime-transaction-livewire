77e851bb204861e95f0ff9aacdff066587ec0a44
abdullohumar <iniautok@gmail.com>
2026-04-07 11:31:32 +0700

    form & validasi

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Dashboard extends Component
@@ -9,6 +10,9 @@ class Dashboard extends Component
     public $name = 'Ndoro Umar';
     public $asset = 1500000; // 1.500.000
     public $transactionCount = 0;
+
+    #[Validate('required|numeric|min:10000', message: 'Minimal top up adalah 10.000')]
+    public $nominal;
 
     public function plusAssets()
     {
@@ -21,6 +25,16 @@ class Dashboard extends Component
             $this->asset -= 500000; // Kurang 500.000
             $this->transactionCount++; // Tambah 1 transaksi
         }
+    }
+    public function topUp()
+    {
+        // Menjalankan validasi berdasarkan #[Validate]
+        $this->validate();
+
+        $this->asset += $this->nominal;
+        $this->transactionCount++;
+
+        $this->reset('nominal');
     }
     public function render()
     {
