55c30c96d8edb87f17b5fad5e93fe52df7365440
abdullohumar <iniautok@gmail.com>
2026-04-07 11:02:32 +0700

    Actions & Method

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -9,6 +9,19 @@ class Dashboard extends Component
     public $name = 'Ndoro Umar';
     public $asset = 1500000; // 1.500.000
     public $transactionCount = 0;
+
+    public function plusAssets()
+    {
+        $this->asset += 500000; // Tambah 500.000
+        $this->transactionCount++; // Tambah 1 transaksi
+    }
+    public function minusAssets()
+    {
+        if ($this->asset >= 500000) {
+            $this->asset -= 500000; // Kurang 500.000
+            $this->transactionCount++; // Tambah 1 transaksi
+        }
+    }
     public function render()
     {
         return view('livewire.dashboard');
