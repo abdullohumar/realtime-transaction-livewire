df740a67775071b820718556ad31e67d61942cfe
abdullohumar <iniautok@gmail.com>
2026-04-08 13:01:20 +0700

    Delete Action & Confirmation

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -33,7 +33,7 @@
         </div>
         <div class="p-8 bg-indigo-600 rounded-2xl border border-indigo-500 text-white shadow-xl shadow-indigo-200">
             <p class="text-indigo-100 text-xs font-bold uppercase tracking-widest text-opacity-70">Progress Belajar</p>
-            <p class="text-3xl font-black mt-2">Bab 7 : Search & Filter Realtime</p>
+            <p class="text-3xl font-black mt-2">Bab 8 : Delete Action & Confirmation</p>
         </div>
     </div>
 
@@ -94,6 +94,7 @@
                         <th class="px-6 py-4 text-xs font-bold text-slate-400 uppercase">Waktu</th>
                         <th class="px-6 py-4 text-xs font-bold text-slate-400 uppercase">Tipe</th>
                         <th class="px-6 py-4 text-xs font-bold text-slate-400 uppercase">Nominal</th>
+                        <th class="px-6 py-4 text-xs font-bold text-slate-400 uppercase">Action</th>
                     </tr>
                 </thead>
                 <tbody class="divide-y divide-slate-50">
@@ -105,6 +106,17 @@
                             </td>
                             <td class="px-6 py-4 font-bold text-slate-700">
                                 {{ $item->type == 'masuk' ? '+' : '-' }} Rp {{number_format($item->amount, 0, ',', '.')}}
+                            </td>
+                            <td class="px-6 py-4">
+                                <button 
+                                wire:click="delete({{$item->id}})"
+                                wire:confirm="Yakin ingin menghapus transaksi ini?"
+                                class="text-slate-400 hover:text-red-500"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                    </svg>
+                                </button>
                             </td>
                         </tr>
                     @empty
